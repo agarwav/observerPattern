27735c105f5c23fc6a495a482a5ea79495e58198
Vaibhav Agarwal <vaibhav.agarwal1991@gmail.com>
2014-05-30 19:49:58 +0530

    Current Condition Display Changes

--- CurrentConditionDisplay.java
+++ CurrentConditionDisplay.java
@@ -8,4 +8,14 @@ public class CurrentConditionDisplay implements Observer,Display {
         this.weatherData = weatherData;
         this.weatherData.registerObserver(this);
     }
+
+    public void update(float temp, float hum, float pres) {
+        this.temp = temp;
+        this.hum = hum;
+        this.pres = pres;
+    }
+
+    public void display() {
+        System.out.println(temp + " " + pres + " " + hum);
+    }
 }
